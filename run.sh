01a63b9a48c800f19fb326ce373cd7f0e0b1440e
Vitaliy Gorbachov <raikerian@gmail.com>
2023-04-08 15:08:43 +0700

    generelize run.sh script

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# This script is meant to be the fastest way of
+# getting started with this application. Set up
+# your API tokens in the variables below, then
+# simply run the script, e.g. `./run.sh`.
+
 set -e
+
+# Set up your API tokens here
+DISCORD_TOKEN=""
+OPENAI_TOKEN=""
 
 IMAGE_NAME="${PWD##*/}"
 
 docker build -t "${IMAGE_NAME}" .
-
-docker run -it --rm --name remai -e DISCORD_TOKEN="$(cat ~/Dev/discord_token)" -e OPENAI_TOKEN="$(cat ~/Dev/openai_token)" "${IMAGE_NAME}"
+docker run -it --rm --name remai -e DISCORD_TOKEN -e OPENAI_TOKEN "${IMAGE_NAME}"
