9f1040ee0378f8a634f3a73f6ed749f4d6215ea8
Vitaliy Gorbachov <raikerian@gmail.com>
2023-05-04 18:42:28 +0700

    improve tiktoken model selection

--- pkg/commands/chat/tiktoken.go
+++ pkg/commands/chat/tiktoken.go
@@ -6,23 +6,29 @@ import (
 )
 
 func countTokens(messages []openai.ChatCompletionMessage, model string) *int {
-	enc, err := tokenizer.ForModel(tokenizer.Model(model))
-	if err != nil {
-		enc, _ = tokenizer.Get(tokenizer.Cl100kBase)
-	}
-
 	var tokensPerMessage int
 	var tokensPerName int
 	switch model {
 	case openai.GPT3Dot5Turbo:
+		// gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301
+		return countTokens(messages, openai.GPT3Dot5Turbo0301)
+	case openai.GPT4:
+		// gpt-4 may change over time. Returning num tokens assuming gpt-4-0314
+		return countTokens(messages, openai.GPT40314)
+	case openai.GPT3Dot5Turbo0301:
 		tokensPerMessage = 4 // every message follows <im_start>{role/name}\n{content}<im_end>\n
 		tokensPerName = -1   // if there's a name, the role is omitted
-	case openai.GPT4:
+	case openai.GPT40314:
 		tokensPerMessage = 3
 		tokensPerName = 1
 	default:
 		// Not implemented
 		return nil
+	}
+
+	enc, err := tokenizer.ForModel(tokenizer.Model(model))
+	if err != nil {
+		enc, _ = tokenizer.Get(tokenizer.Cl100kBase)
 	}
 
 	tokens := 0
