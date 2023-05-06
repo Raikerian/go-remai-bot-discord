696b1653a1259ff18e186657256cb9f7a06d81bf
Vitaliy Gorbachov <raikerian@gmail.com>
2023-05-06 18:41:33 +0700

    Increase base gpt cache size to 64 threads

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 const (
 	Version = "0.4.5"
 
-	DiscordThreadsCacheSize = 32
+	DiscordThreadsCacheSize = 64
 
 	OpenAIBlackIconURL = "https://ph-files.imgix.net/b739ac93-2899-4cc1-a893-40ea8afde77e.png"
 )
